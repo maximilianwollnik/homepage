a208fde7dcbb6002ecde7a1eb6fa97d6d526fddf
maximilianwollnik <maximilianwollnik@users.noreply.github.com>
2015-10-12 22:56:01 +0200

    add: more tests for mail contact
    - added more unit tests
    - added e2e tests

--- src/test/webapp/unit/app/shared/social/socialController.spec.js
+++ src/test/webapp/unit/app/shared/social/socialController.spec.js
@@ -1,8 +1,7 @@
 'use strict';
 
 describe('homepage.social', function() {
-  var scope, ctrl, form, templateHtml, formElem, dirElementInput;
-  var $compile, $rootScope, $httpBackend;
+  var scope, ctrl, form, templateHtml, formElem, $compile, $rootScope, $httpBackend, elemName, elemMail, elemBody;
   beforeEach(module('homepage'));
   // load the templates
   beforeEach(module('templates'));
@@ -13,20 +12,24 @@ describe('homepage.social', function() {
     $httpBackend = _$httpBackend_;
     jasmine.getJSONFixtures().fixturesPath = 'base/src/main/webapp/assets/i18n';
 
-    $httpBackend.whenGET('assets/i18n/de_DE.json').respond(
-        getJSONFixture('de_DE.json'));
-    $httpBackend.whenGET('assets/i18n/C.json').respond(
-        getJSONFixture('de_DE.json'));
-    
+    $httpBackend.whenGET('assets/i18n/de_DE.json').respond(getJSONFixture('de_DE.json'));
+    $httpBackend.whenGET('assets/i18n/C.json').respond(getJSONFixture('de_DE.json'));
+
     scope = $rootScope;
     ctrl = $controller('SocialCtrl', {
       $scope : scope
     });
-    
+
     var element = angular.element(_$templateCache_.get('app/shared/social/socialDirectiveView.html'));
     $compile(element)(scope);
     formElem = element.find('form');
+    elemName = element.find('input[name*="name"]');
+    elemMail = element.find('input[name*="email"]');
+    elemBody = element.find('textarea[name*="body"]');
     $compile(formElem)(scope);
+    $compile(elemName)(scope);
+    $compile(elemMail)(scope);
+    $compile(elemBody)(scope);
     form = scope.mailForm;
   }));
 
@@ -35,20 +38,87 @@ describe('homepage.social', function() {
     it('and it must be defined', function() {
       expect(ctrl).toBeDefined();
     });
-    
+
     it('and the form must be valid, when the correct entries are included', function() {
 
       form.name.$setViewValue('name');
       form.email.$setViewValue('name@domain.de');
       form.body.$setViewValue('text');
       scope.$apply();
-      $rootScope.$digest();      
-      
-//      angular.element(dirElementInput).val('name@domain.de').trigger('input');
-      //scope.submit(form);
-//      expect(dirElementInput.hasClass('ng-valid')).toEqual(true);
-//      alert(form);
+      $rootScope.$digest();
+
+      expect(angular.element(elemName).hasClass('ng-valid')).toEqual(true);
+      expect(angular.element(elemMail).hasClass('ng-valid')).toEqual(true);
+      expect(angular.element(elemBody).hasClass('ng-valid')).toEqual(true);
+
       expect(form.$valid).toEqual(true);
+    });
+    
+    it('and the form must be invalid, when the input name is empty', function() {
+
+      form.name.$setViewValue('');
+      form.email.$setViewValue('name@domain.de');
+      form.body.$setViewValue('text');
+      scope.$apply();
+      $rootScope.$digest();
+
+      expect(angular.element(elemName).hasClass('ng-invalid')).toEqual(true);
+      expect(angular.element(elemMail).hasClass('ng-valid')).toEqual(true);
+      expect(angular.element(elemBody).hasClass('ng-valid')).toEqual(true);
+
+      expect(form.$valid).toEqual(false);
+    });
+    
+    it('and the form must be invalid, when the input mail is empty', function() {
+
+      form.name.$setViewValue('name');
+      form.email.$setViewValue('');
+      form.body.$setViewValue('text');
+      scope.$apply();
+      $rootScope.$digest();
+
+      expect(angular.element(elemName).hasClass('ng-valid')).toEqual(true);
+      expect(angular.element(elemMail).hasClass('ng-invalid')).toEqual(true);
+      expect(angular.element(elemBody).hasClass('ng-valid')).toEqual(true);
+
+      expect(form.$valid).toEqual(false);
+    });
+    
+    it('and the form must be invalid, when the input body is empty', function() {
+
+      form.name.$setViewValue('name');
+      form.email.$setViewValue('name@domain.de');
+      form.body.$setViewValue('');
+      scope.$apply();
+      $rootScope.$digest();
+
+      expect(angular.element(elemName).hasClass('ng-valid')).toEqual(true);
+      expect(angular.element(elemMail).hasClass('ng-valid')).toEqual(true);
+      expect(angular.element(elemBody).hasClass('ng-invalid')).toEqual(true);
+
+      expect(form.$valid).toEqual(false);
+    });
+    
+    it('and the form must be invalid, when the input email is invalid', function() {
+
+      form.email.$setViewValue('name');
+      scope.$apply();
+      $rootScope.$digest();
+
+      expect(angular.element(elemMail).hasClass('ng-invalid')).toEqual(true);
+      
+      form.email.$setViewValue('name@');
+      scope.$apply();
+      $rootScope.$digest();
+
+      expect(angular.element(elemMail).hasClass('ng-invalid')).toEqual(true);
+      
+      form.email.$setViewValue('@asd');
+      scope.$apply();
+      $rootScope.$digest();
+
+      expect(angular.element(elemMail).hasClass('ng-invalid')).toEqual(true);
+
     });
 
   });
