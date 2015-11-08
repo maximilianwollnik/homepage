c7aed6c634b1ccce5bbafcea9f4e2ef96d87107d
maximilianwollnik <maximilian@maximilianwollnik.de>
2015-11-08 17:13:13 +0100

    change: new check for release
    - string compare did not work, next try

--- releaseCheck.sh
+++ releaseCheck.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-payload=$1
 branch=$(echo $payload | jq '.ref')
 headCommit=$(echo $payload | jq '.head_commit.message')
 commitToCompare=maven-release-plugin
 
-if [ $branch != \"refs/heads/master\" ] || [ "${headCommit/$commitToCompare}" = "$headCommit" ] ; then 
+if [ $branch != \"refs/heads/master\" ] || [ "${headCommit#*$commitToCompare}" != "$headCommit" ] ; then 
   echo no
   exit 1
 fi
