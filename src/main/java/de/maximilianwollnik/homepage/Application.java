9d2b2c9bdfc704346db0d1b7d824fd7f130f20a1
Maximilian <maximilian@maximilianwollnik.de>
2016-01-05 22:47:26 +0100

    reducing log size
    - remove package "gauge.servo" from metrics

--- src/main/java/de/maximilianwollnik/homepage/Application.java
+++ src/main/java/de/maximilianwollnik/homepage/Application.java
@@ -6,6 +6,7 @@ package de.maximilianwollnik.homepage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -21,6 +22,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@RefreshScope
 public class Application {
 
   /**
