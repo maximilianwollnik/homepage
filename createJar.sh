c27be40477bad48b8375b01f3e511490bee080ea
maximilianwollnik <maximilian@maximilianwollnik.de>
2022-08-09 17:31:15 +0200

    fix: updating debian

--- createJar.sh
+++ createJar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export NG_CLI_ANALYTICS=ci
 
+apt-get dist-upgrade
+sed -i -e 's/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
+apt-get update
 apt-get install -y curl
 curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
