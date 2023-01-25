4da55ed53c9004248b442beb7f99fdec995b18c7
maximilianwollnik <maximilian@maximilianwollnik.de>
2023-01-25 21:56:09 +0100

    - update after docker update

--- 03_runDocker.sh
+++ 03_runDocker.sh
@@ -2,5 +2,5 @@ function prop {
     grep "${1}" gradle.properties|cut -d'=' -f2
 }
 
-docker run -d --name homepage -v /mnt/data/scripts/docker/homepage:/tmp --network spring -p 9000:9000 -w /tmp maximilianwollnik/homepage:$(prop 'version') /bin/bash /tmp/gradlew bootRun
+docker run -d --restart unless-stopped --name homepage -v /mnt/data/scripts/docker/homepage:/tmp --network spring -p 9000:9000 -w /tmp maximilianwollnik/homepage:$(prop 'version') /bin/bash /tmp/gradlew bootRun
 #docker run -d --name homepage --network spring maximilianwollnik/homepage:$(prop 'version')
--- createJar.sh
+++ createJar.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 export NG_CLI_ANALYTICS=ci
 
-apt-get dist-upgrade
-sed -i -e 's/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
-apt-get update
+apt-get dist-upgrade -y
 apt-get install -y curl
 curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
-apt-get install -y nodejs
+apt-get install -y nodejs npm
 
 cd /usr/local/homepage/
 /bin/bash /usr/local/homepage/gradlew bootJar --no-daemon
